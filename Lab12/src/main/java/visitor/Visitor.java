d7b9d41368020fa84cb598685b9c12663d218719
Zavodnik10 <116554137+Zavodnik10@users.noreply.github.com>
2024-10-13 16:18:51 +0300

    Delete Lab12 directory

--- Lab12/src/main/java/visitor/Visitor.java
+++
@@ -1,9 +0,0 @@
-package visitor;
-
-import java.util.Map;
-
-public interface Visitor<T> {
-    Map<String, String> onSignature(Task<T> task);
-    Map<String, String> onGroupStart(Group<T> task);
-    void onGroupEnd(Group<T> task);
-}
